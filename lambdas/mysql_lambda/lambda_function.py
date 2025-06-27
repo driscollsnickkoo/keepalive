71033008a8809eba39f2759f8248aeb7d5fe52d3
Nick Koo <nick.koo@driscolls.com>
2025-06-27 11:21:32 +0100

    with mysql lambda

---
+++ lambdas/mysql_lambda/lambda_function.py
@@ -0,0 +1,35 @@
+import os
+import pymysql
+
+# Read environment variables
+DB_HOST = os.environ['DB_HOST']
+DB_PORT = int(os.environ['DB_PORT'])
+DB_USER = os.environ['DB_USER']
+DB_PASS = os.environ['DB_PASS']
+DB_NAME = os.environ['DB_NAME']
+
+def lambda_handler(event, context):
+    try:
+        conn = pymysql.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS,
+            database=DB_NAME,
+            port=DB_PORT,
+            connect_timeout=5
+        )
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT VERSION();")
+            version = cursor.fetchone()
+            print("Database version:", version)
+        conn.close()
+        return {
+            "statusCode": 200,
+            "body": f"Connected to MySQL, version: {version}"
+        }
+    except Exception as e:
+        print("ERROR:", e)
+        return {
+            "statusCode": 500,
+            "body": str(e)
+        }
