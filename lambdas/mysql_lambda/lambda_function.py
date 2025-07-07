b4590f1603d877f700bbe67c2a903c05a38e3430
Nick Koo <nick.koo@driscolls.com>
2025-07-07 13:50:36 +0100

    just sql

--- lambdas/mysql_lambda/lambda_function.py
+++ lambdas/mysql_lambda/lambda_function.py
@@ -1,6 +1,5 @@
 import os
 import pymysql
-import boto3
 import json
 
 # Read environment variables for mysql
@@ -10,75 +9,32 @@ DB_USER = os.environ['DB_USER']
 DB_PASS = os.environ['DB_PASS']
 DB_NAME = os.environ['DB_NAME']
 
-
-# SNS config
-sns_topic_arn = 'arn:aws:sns:eu-north-1:825765396866:sns_nktest'
-
-sns_client = boto3.client('sns')
+# Uncomment below only if you want to test sns later
+# import boto3
+# sns_topic_arn = 'arn:aws:sns:eu-north-1:825765396866:sns_nktest'
+# sns_client = boto3.client('sns')
 
 def lambda_handler(event, context):
     try:
+        # Attempt to connect to MySQL
         conn = pymysql.connect(
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASS,
             database=DB_NAME,
             port=DB_PORT,
-            connect_timeout=100
+            connect_timeout=10
         )
-        with conn.cursor() as cursor:
-            query = """
-                    select * from T_Inspection_UK;
-                    """
-            cursor.execute(query)
-            rows = cursor.fetchall()
-            
-        if rows:
-            msg_lines = []
-            for row in rows:
-                line = f"Record: {row[0]}, PalletID: {row[1]}, Submitted: {row[2]}, BotStatus: {row[4]}"
-                msg_lines.append(line)
+        # Connection successful
+        conn.close()
+        return {
+            'statusCode': 200,
+            'body': json.dumps('MySQL connection successful.')
+        }
 
-            message = "\n".join(msg_lines)
-            message = "NK is testing SNS"
-            
-            sns_response = sns_client.publish(
-                TopicArn=sns_topic_arn,
-                Subject="NK test sns subject updated with eu-north-1 ",
-                Message=f"Found NK {message}"
-            )
-
-
-            
-            return {
-                'statusCode': 200,
-                'body': json.dumps({
-                    'status': 'just mysql',
-                    'messageId': sns_response.get('MessageId'),
-                    'message': message
-                    # ,
-                    # 'rowsFound': len(rows)
-                })
-            }
-        else:
-            
-            sns_response = sns_client.publish(
-                TopicArn=sns_topic_arn,
-                Subject="NK no entry in testqmp 2025-07-07 ",
-                Message="Nothing Found NK"
-            )
-            return {
-                'statusCode': 200,
-                'body': json.dumps('No unprocessed records found.')
-            }
-
-
-        
-
-    
     except Exception as e:
         print("ERROR:", e)
         return {
             'statusCode': 500,
-            'body': json.dumps(f'Error: {str(e)}')
+            'body': json.dumps(f'Error connecting to MySQL: {str(e)}')
         }
